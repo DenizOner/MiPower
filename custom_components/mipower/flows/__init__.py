b56d681d33ab1f9a0461258bb1ca48923b38bf65
R. Deniz Öner <12309838+DenizOner@users.noreply.github.com>
2025-11-17 13:28:44 +0300

    Delete custom_components/mipower directory

--- custom_components/mipower/flows/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-This empty file makes the 'flows' directory a Python package.
-
-This allows us to import modules from this directory using dot notation,
-for example: from .flows.options import ...
-"""
